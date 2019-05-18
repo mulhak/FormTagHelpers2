ca3067786e24787402c5491753582dc560b3766b
mulhak <mulhakieran@gmail.com>
2019-05-18 16:34:10 +0100

    renames veg List to ListVeggies

--- FormTagHelpers2/Models/RepositoryVeg.cs
+++ FormTagHelpers2/Models/RepositoryVeg.cs
@@ -8,22 +8,23 @@ namespace FormTagHelpers2.Models
     public interface IRepositoryVeg
     {
         IEnumerable<Veg> Veges { get; }
+
         void AddVeg(Veg veggi);
     }
     public class VegRepo : IRepositoryVeg
     {
-        private List<Veg> veggies => new List<Veg>
+        private List<Veg> ListVeggies => new List<Veg>
         {
             new Veg {Name= "Carrot", Price = 1.80M, Qty= "5"},
             new Veg {Name= "Cabbage", Price= 1.50M, Qty= "2"},
             new Veg {Name = "Celery", Price = 2.20M, Qty= "3"}
 
         };
-        public IEnumerable<Veg> Veges => veggies;
+        public IEnumerable<Veg> Veges => ListVeggies;
 
         public void AddVeg(Veg veggi)
         {
-            throw new NotImplementedException();
+            ListVeggies.Add(veggi);
         }
     }
 }
